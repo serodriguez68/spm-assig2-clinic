fafeefe806dbc7c4cf448d68ccfa8797cac57f4f
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-04-20 19:56:34 +1000

    Req 2: create Specialty model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-
-
 # Create admin user
 admin_user = User.create!(email: 'alena@alenaclinic.com', name: 'Alena', password: 123456789, password_confirmation: 123456789)
 admin_user.admin!
 puts 'CREATED ADMIN USER: ' << admin_user.email
+
+# Specialties
+Specialty.create!(name: 'Podiatrist')
+Specialty.create!(name: 'Naturopath')
+Specialty.create!(name: 'Chiropractor')
+
